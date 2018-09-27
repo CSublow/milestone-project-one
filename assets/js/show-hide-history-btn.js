78fa4a939df902e75e127246d3a2425dff0d3bbf
Chris Hengmith <crhengmith@hotmail.co.uk>
2018-09-27 14:49:05 +0000

    Formatted history section in index.html so the first paragraph visible on xs screens

--- assets/js/show-hide-history-btn.js
+++ assets/js/show-hide-history-btn.js
@@ -1,7 +1,8 @@
+/* This script hides most of the history text on smaller screens, displaying the full history text only to users who want to read it */
 $(function() {
     $("#history-show-hide-btn").click(function () {
         $(this).text(function(i, text) {
-            return text === "Click to Read" ? "Hide" : "Click to Read";
+            return text === "Read More..." ? "Close" : "Read More...";
         })
     });
 })
