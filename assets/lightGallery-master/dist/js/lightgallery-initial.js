4d7c15c0b2306df46543cc3e06be1046a58aaf97
Chris Hengmith <crhengmith@hotmail.co.uk>
2018-10-10 11:32:09 +0000

    Removed embedded lightgallery js and placed in lightgallery-initial.js, added script src

---
+++ assets/lightGallery-master/dist/js/lightgallery-initial.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#aniimated-thumbnials').lightGallery({
+        thumbnail: true
+    });
+    $('#aniimated-thumbnials-2').lightGallery({
+        thumbnail: true
+    });
+});
