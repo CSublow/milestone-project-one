7164f9df7501945891e90e99232cdb34cb7bf36f
Chris Hengmith <crhengmith@hotmail.co.uk>
2018-10-11 10:19:57 +0000

    Deleted show-hide-history-btn.js

--- assets/js/show-hide-history-btn.js
+++
@@ -1,8 +0,0 @@
-/* This script changes the text of the show/hide history button to match the history text's current hidden/visible state */
-$(function() {
-    $("#history-show-hide-btn").click(function () {
-        $(this).text(function(i, text) {
-            return text === "Read More..." ? "Close" : "Read More...";
-        })
-    });
-})
